53dbbb9995c116b8926393b24eda612279104151
shlokburmi <135860207+shlokburmi@users.noreply.github.com>
2026-06-22 15:18:08 +0530

    Hello world

---
+++ Booking-App/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // fmt or format alows print function
+
+// func is entry point to code so one main.go file can have only one main func
+// go programs are organised in packages
+
+func main() {
+	fmt.Print("Hiiii..")
+	fmt.Println("Hello World")
+}
